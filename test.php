5b18e69cb4ae849daa529fe61c730c1724187fae
MasonGallagher <Masongallagher90@gmail.com>
2020-11-25 16:07:38 +0000

    Added variable pack behaviour

--- test.php
+++ test.php
@@ -1,59 +1,80 @@
 <?php 
 	
-	$packs = array(250,500,1000,2000,5000);
-	$order=12001;
+	$packs = array(250,300,500,1999,2000,5000);
 	echo "Packs: ". json_encode($packs) . "<br><br>";
 	
-	getPacks($packs, $order);
-	
-	$order=501;
-	getPacks($packs, $order);
-	
-	$order=501;
-	getPacks($packs, $order);
-
-	$order=251;
+	$order=1;
 	getPacks($packs, $order);
 	
 	$order=250;
 	getPacks($packs, $order);
 	
-	$order=1;
+	$order=251;
 	getPacks($packs, $order);
+	
+	$order=501;
+	getPacks($packs, $order);
+	
+	$order=251;
+	getPacks($packs, $order);
+	
+	$order=2001;
+	getPacks($packs, $order);
+	
+	$order=12001;
+	getPacks($packs, $order);
+	
+	
 	
 	function getPacks($packs, $order){
 		echo "Order: " . ($order) . "<br>";
-		
-		$cart = calculateCart($packs, $order);
-		$cartSum=0;
-		for ($i = 0; $i<count($cart); $i++) {
-			$cartSum += $cart[$i];
-		}
-		
-		echo json_encode(calculateCart($packs, $cartSum)) . "<br> <br>";
+		$cart=array();
+		echo json_encode(sortPacks($cart, $packs, $order)) . "<br> <br>";
 	}
 	
-	function calculateCart($packs, $order){
-		$cart=array();
-		for ($i = count($packs)-1; $i >= 0; $i--) {
-			$pack=$packs[$i];
-			while(true){
-				$sum = $order - $pack;
-				if($sum >= 0){
-					$order = $order - $pack;
-					if($order < 0){
-						break;
-					}
-					array_push($cart,$pack); 
-				}else{
-					break;
+	function sortPacks($cart, $packs, $order){
+	    $newCart = array();
+		$div = 10000000;
+		$oldItem = 0;
+		$closeness = -10000;
+		for($i =0; $i < count($packs); $i++){
+			$item = $packs[$i];
+            $newDiv = floor($order/$item);
+            if($newDiv <= $div && $newDiv != 0){
+				$div = $newDiv;
+				$oldItem = $item;
+				$closeness = -$item;
+			}else if($newDiv == 0){
+				if($closeness < ($order - $item)){
+					$div = 1;
+					$oldItem = $item;
+					$closeness = $order - $item;
 				}
+				
+            }else{
+				break;
+			}
+        }
+	
+		if($oldItem!=0){
+			for($j =0; $j < $div; $j++){
+				array_push($newCart,$oldItem); 
 			}
 		}
-		if($order >= 1){
-			array_push($cart,$packs[0]); 
+		$newTotal = array_sum($newCart);
+		$newTotal = $order - $newTotal;
+
+        if($newTotal>0){
+			if($newTotal<$packs[0]){
+				$cart = array_merge($cart,$newCart); 
+				array_push($cart,$packs[0]); 
+			}else{
+				$cart = array_merge($cart,sortPacks($newCart, $packs, $newTotal)); 
+			}
+		}else{
+			$cart = array_merge($cart,$newCart); 
 		}
-		return $cart;
+		return $cart;	
 	}
 	
 	
